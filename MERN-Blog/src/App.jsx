f4a9b2fd29158f299b966458c2f2d7ba8036a04b
Nimnapathum <pathumeshan123@gmail.com>
2024-06-28 11:12:12 +0530

    Update to the pages and routes

--- MERN-Blog/src/App.jsx
+++ MERN-Blog/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <div><h1 className='text-3xl text-red-500'>App</h1></div>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home />} />  
+      <Route path='/about' element={<About />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/projects' element={<Projects />} />
+      </Routes>   
+    
+    </BrowserRouter>
   )
 }
